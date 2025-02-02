d1214914a8735c8896b83098fba8be575e022057
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2025-02-02 17:13:29 +0200

    header visual

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,17 @@
-import { Link } from "react-router-dom";
-
-import Container from "../Container/Container";
-
-const Header = () => (
-  <header>
-    <Container>
-      <nav>
-        <Link to="welcome">LogoTracker</Link> | <Link to="signin">Sign In</Link>
-      </nav>
-    </Container>
-  </header>
-);
-
-export default Header;
-/*import React from "react";
-import css from "./Header.css";
-import logo from "../../assets/icons";
+import React from "react";
+import Logo from "../Logo/Logo"; // Підключаємо Logo
+import UserAuth from "../UserAuth/UserAuth"; // Підключаємо UserAuth
+import styles from "./Header.module.css";
 
 const Header = () => {
-  const username = "David"; // Отримати з Redux або бекенду
-  const userAvatar = "https://randomuser.me/api/portraits/men/45.jpg"; // Тестова картинка
-
   return (
-    <HeaderWrapper>
-      <Logo className={css.logo} src={logo} alt="Tracker of Water" />
-      <UserSection>
-        <span>{username}</span>
-        <Avatar src={userAvatar} alt="User Avatar" />
-      </UserSection>
-    </HeaderWrapper>
+    <header>
+      <nav className={styles.header}>
+        <Logo /> {/* Компонент логотипу */}
+        <UserAuth /> {/* Компонент авторизації */}
+      </nav>
+    </header>
   );
 };
 
-export default Header;*/
+export default Header;
