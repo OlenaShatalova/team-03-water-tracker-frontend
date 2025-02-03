e94a77a1d127ed7ae96f3f6b9b04c59437adda54
Hanna Bondarenko <annaleshhuk@gmail.com>
2025-02-03 21:04:49 +0200

    add ThemeContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
 // import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
-import "modern-normalize";
-import "./index.css";
-import App from "./App.jsx";
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { ThemeProvider } from './components/ThemeContext/ThemeContext.jsx';
 
-createRoot(document.getElementById("root")).render(
+import 'modern-normalize';
+import './index.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </PersistGate>
   </Provider>
