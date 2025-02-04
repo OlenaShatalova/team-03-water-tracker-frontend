e8ef2ce0bcadb4a4cb5059b2defda483cc6e1b24
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2025-02-04 10:51:07 +0300

    add loader, modify app and layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,47 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import RestrictedRoute from './components/RestrictedRoute';
 import PrivateRoute from './components/PrivateRoute';
-
+import LoaderFallback from './components/LoaderFallback/LoaderFallback';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 
 const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
-// const SignupPage = lazy(() => import("./pages/SignupPage/SignupPage"));
-// const SigninPage = lazy(() => import("./pages/SigninPage/SigninPage"));
-// const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const SignupPage = lazy(() => import('./pages/SignupPage/SignupPage'));
+const SigninPage = lazy(() => import('./pages/SigninPage/SigninPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 
 // import MainPage from "./pages/MainPage/MainPage";
-import SignupPage from './pages/SignupPage/SignupPage';
-import SigninPage from './pages/SigninPage/SigninPage';
-import HomePage from './pages/HomePage/HomePage';
+// import SignupPage from './pages/SignupPage/SignupPage';
+// import SigninPage from './pages/SigninPage/SigninPage';
+// import HomePage from './pages/HomePage/HomePage';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Navigate to="/welcome" />} />
-        <Route
-          path="welcome"
-          element={
-            <Suspense fallback={<div>Add LOADER...</div>}>
-              <MainPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="signup"
-          element={
-            <Suspense fallback={<div>Add LOADER...</div>}>
+    <Suspense fallback={<LoaderFallback />}>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Navigate to="/welcome" />} />
+          <Route path="welcome" element={<MainPage />} />
+          <Route
+            path="signup"
+            element={
               <RestrictedRoute redirectTo="/home" component={<SignupPage />} />
-            </Suspense>
-          }
-        />
-        <Route
-          path="signin"
-          element={
-            <Suspense fallback={<div>Add LOADER...</div>}>
+            }
+          />
+          <Route
+            path="signin"
+            element={
               <RestrictedRoute redirectTo="/home" component={<SigninPage />} />
-            </Suspense>
-          }
-        />
-        <Route
-          path="home"
-          element={
-            <Suspense fallback={<div>Add LOADER...</div>}>
+            }
+          />
+          <Route
+            path="home"
+            element={
               <PrivateRoute redirectTo="/signin" component={<HomePage />} />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
+            }
+          />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
