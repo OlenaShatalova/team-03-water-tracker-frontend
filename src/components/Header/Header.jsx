c52f2fd3f11da31bb344480ea800c26f66358962
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-02-06 16:17:31 +0200

    change input, add closeModal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 import { ReactSVG } from 'react-svg';
 
 import Container from '../Container/Container';
-import UserAuth from '../UserAuth/UserAuth'; // Підключаємо UserAuth
+// import UserAuth from '../UserAuth/UserAuth'; // Підключаємо UserAuth
 
 import logo from '../../assets/icons/logo.svg';
 import iconMen from '../../assets/icons/user.svg';
 
 import css from './Header.module.css';
+
+// import SettingModal from '../SettingModal/SettingModal';
 
 /*<UserAuth />;*/
 const Header = () => {
@@ -18,6 +20,10 @@ const Header = () => {
           <Link to="welcome">
             <ReactSVG src={logo} className={css.logo} />
           </Link>
+          {/* <button type="button" onClick={onOpenModal}>
+            Settings
+          </button>
+          {isModalOpen && <SettingModal onCloseModal={onCloseModal} />} */}
           <Link to="signin" className={css.signIn}>
             Sign In
             <ReactSVG src={iconMen} className={css.iconMen} />
--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,38 +1,82 @@
-import { Field, Form, Formik } from 'formik';
 import css from './SettingModal.module.css';
+
+import { Form, Formik } from 'formik';
+import { ReactSVG } from 'react-svg';
+import { useEffect } from 'react';
+// import { useState } from 'react';
 
 import UserAvatar from './UserAvatar/UserAvatar';
 
-import { ReactSVG } from 'react-svg';
 import close from '../../assets/icons/close.svg';
 import upload from '../../assets/icons/upload.svg';
 
 import GenderIdentity from './GenderIdentity/GenderIdentity';
 import PwdSection from './PwdSection/PwdSection';
+import { UserInfoSchema } from '../../utils/schemas/UserInfoSchema';
+import Input from '../Input/Input';
 
-const SettingModal = () => {
-  const INITIAL_VALUES = {
-    photo: '',
-    name: '',
-    email: '',
-    password: '',
-  };
+const INITIAL_VALUES = {
+  gender: 'man',
+  name: '',
+  email: '',
+  oldpassword: '',
+  password: '',
+};
+
+const SettingModal = ({ onCloseModal }) => {
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // const onOpenModal = () => {
+  //   setIsModalOpen(true);
+  //   document.body.style.overflow = 'hidden';
+  // };
+
+  // const onCloseModal = () => {
+  //   setIsModalOpen(false);
+  //   document.body.style.overflow = 'auto';
+  // };
+
+  useEffect(() => {
+    const onKeyDown = evt => {
+      if (evt.code === 'Escape') {
+        onCloseModal();
+      }
+    };
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onCloseModal]);
 
   const handleSubmit = values => {
     console.log(values);
   };
+  const onBackdropClick = evt => {
+    if (evt.target === evt.currentTarget) {
+      onCloseModal();
+    }
+  };
 
   return (
-    <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
-      <div className={css.backdrop}>
+    <Formik
+      initialValues={INITIAL_VALUES}
+      validationSchema={UserInfoSchema}
+      onSubmit={handleSubmit}
+    >
+      <div className={css.backdrop} onClick={onBackdropClick}>
         <Form className={css.modal}>
           <div className={css.titleWrapper}>
-            <h2>Setting</h2>
-            <button type="button" className={css.closeBtn}>
+            <h2 className={css.mainTtl}>Setting</h2>
+            <button
+              type="button"
+              className={css.closeBtn}
+              onClick={onCloseModal}
+            >
               <ReactSVG src={close} className={css.closeIcon} />
             </button>
           </div>
-          <h3 className={css.blockTtl}>Your photo</h3>
+          <h3 className={css.photoTtl}>Your photo</h3>
           <div className={css.photoWrapper}>
             <UserAvatar />
             <a href="" className={css.uploadLink}>
@@ -40,35 +84,13 @@ const SettingModal = () => {
               <span className={css.uploadText}>Upload a photo</span>
             </a>
           </div>
-          <div className={css.userDetailsWrapper}>
-            <div>
+          <div className={css.userDetailsAndPwd}>
+            <div className={css.userDetailsWrapper}>
               <GenderIdentity />
-              <div className={css.sectWrapper}>
-                <h3 className={css.blockTtl}>Your name</h3>
-                <label>
-                  <Field
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    className={css.textInput}
-                  />
-                </label>
-              </div>
-              <div className={css.sectWrapper}>
-                <h3 className={css.blockTtl}>Email</h3>
-                <label>
-                  <Field
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                    className={css.textInput}
-                  />
-                </label>
-              </div>
+              <Input name="name" label="Your name" placeholder="Name" />
+              <Input name="email" label="Email" placeholder="Email" />
             </div>
-            <div>
-              <PwdSection />
-            </div>
+            <PwdSection className={css.pwdWrapper} />
           </div>
           <div className={css.saveBtnWrpr}>
             <button className={css.saveBtn}>Save</button>
