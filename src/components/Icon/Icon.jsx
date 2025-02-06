41ca269cf7e473fbbd0ac4c1cee411d631c3b404
Hanna Bondarenko <annaleshhuk@gmail.com>
2025-02-06 18:47:35 +0200

    Збереження змін перед ребейзом

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,23 +1,19 @@
-const Icon = ({
-  name,
-  width = 24,
-  height = 24,
-  color = 'currentColor',
-  className,
-  ariaLabel,
-}) => {
+import PropTypes from 'prop-types';
+
+const Icon = ({ name, width = 24, height = 24, color = 'currentColor' }) => {
   return (
-    <svg
-      width={width}
-      height={height}
-      fill={color}
-      className={className}
-      role="img"
-      aria-label={ariaLabel || name}
-    >
-      <use xlinkHref={`/assets/images/icons.svg#icon-${name}`} />
+    <svg width={width} height={height} fill={color} className="icon">
+      <use href={`/icons/icons.svg#${name}`} />
     </svg>
   );
+};
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  color: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default Icon;
