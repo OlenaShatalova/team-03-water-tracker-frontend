c1a755c71beea67182a3e245605e41b10cac9000
Olena Shatalova <08082023m@gmail.com>
2025-02-07 14:34:59 +0200

    attempt for render

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,55 +1,57 @@
-import { useDispatch, useSelector } from "react-redux";
-import { openModal, closeModal } from "../../redux/modal/slice";
-import DailyWaterIntake from "../DailyWaterIntake/DailyWaterIntake";
-import AddWaterModal from "../AddWaterModal/AddWaterModal";
+import { useDispatch, useSelector } from 'react-redux';
+import { openModal, closeModal } from '../../redux/modal/slice';
+import DailyWaterIntake from '../DailyWaterIntake/DailyWaterIntake';
+import AddWaterModal from '../AddWaterModal/AddWaterModal';
 
-import { selectWaterRecordsToday } from "../../redux/today/selectors";
-import { selectIsAddWaterModalOpen } from "../../redux/modal/selectors";
+import { selectWaterRecordsToday } from '../../redux/today/selectors';
+// import { selectIsAddWaterModalOpen } from "../../redux/modal/selectors";
 
 import { ReactSVG } from 'react-svg';
 import plus from '../../assets/icons/plus.svg';
 
-import css from "./TodayWaterList.module.css";
-
+import css from './TodayWaterList.module.css';
 
 const TodayWaterList = () => {
-    const dispatch = useDispatch();
-    const isOpen = useSelector(selectIsAddWaterModalOpen);
+  const dispatch = useDispatch();
+  // const isOpen = useSelector(selectIsAddWaterModalOpen);
+  const isOpen = false;
 
-    const waterRecords = useSelector(selectWaterRecordsToday);
+  const waterRecords = useSelector(selectWaterRecordsToday);
 
-    const handleOpenModal = () => {
-        dispatch(openModal("isAddWaterOpen"));
-    };
+  const handleOpenModal = () => {
+    dispatch(openModal('isAddWaterOpen'));
+  };
 
-    const handleCloseModal = () => {
-        dispatch(closeModal("isAddWaterOpen"));
-    };
+  const handleCloseModal = () => {
+    dispatch(closeModal('isAddWaterOpen'));
+  };
 
-    return (
-        <div className={css.todayWaterListSectionWrapper}>
-            <h2 className={css.todayWaterListHeader}>Today</h2>
+  return (
+    <div className={css.todayWaterListSectionWrapper}>
+      <h2 className={css.todayWaterListHeader}>Today</h2>
 
-            <div className={css.todayWaterListWrapper}>
-                {waterRecords.length > 0 && (
-                    <ul className={css.scrollableList}>
-                        {waterRecords.map(({ _id, waterVolume, time }) => (
-                            <li className={css.waterRecordDataWrapper} key={_id}>
-                                <DailyWaterIntake record={{ id: _id, volume: waterVolume, time: time }} />
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
+      <div className={css.todayWaterListWrapper}>
+        {waterRecords.length > 0 && (
+          <ul className={css.scrollableList}>
+            {waterRecords.map(({ _id, waterVolume, time }) => (
+              <li className={css.waterRecordDataWrapper} key={_id}>
+                <DailyWaterIntake
+                  record={{ id: _id, volume: waterVolume, time: time }}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
 
-            <button className={css.todayWaterListBtn} onClick={handleOpenModal}>
-                <ReactSVG src={plus} className={css.plusIcon} />
-                Add water
-            </button>
+      <button className={css.todayWaterListBtn} onClick={handleOpenModal}>
+        <ReactSVG src={plus} className={css.plusIcon} />
+        Add water
+      </button>
 
-            {isOpen && <AddWaterModal onClose={handleCloseModal} />}
-        </div>
-    );
+      {isOpen && <AddWaterModal onClose={handleCloseModal} />}
+    </div>
+  );
 };
 
 export default TodayWaterList;
