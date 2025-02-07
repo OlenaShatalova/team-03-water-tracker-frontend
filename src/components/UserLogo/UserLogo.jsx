9e87d2cf0988ae1fd02328ac1aa0c3a0ec416cdb
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2025-02-07 15:27:44 +0200

    userAuth

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../redux/auth/selectors';
+
 import { Link } from 'react-router-dom';
 import { ReactSVG } from 'react-svg';
 
@@ -9,7 +12,8 @@ import dropdownIcon from '../../assets/icons/jhh.svg';
 
 import css from './UserLogo.module.css';
 
-const UserLogo = ({ user }) => {
+const UserLogo = () => {
+  const user = useSelector(selectUser);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleModal = () => {
@@ -17,32 +21,23 @@ const UserLogo = ({ user }) => {
   };
 
   const getUserAvatar = () => {
-    // if (user && user.avatar) {
-    //   return user.avatar;
-    // } else {
-    //   const letter = user?.name
-    //     ? user.name[0].toUpperCase()
-    //     : user.email[0].toUpperCase();
-    //   return letter;
-    // }
-    /*<div className={css.avatarPlaceholder}>{letter}</div>;*/
+    if (user && user.avatar) {
+      return <img src={user.avatar} alt="User avatar" className={css.avatar} />;
+    }
+    const letter = user?.name
+      ? user.name[0].toUpperCase()
+      : user.email[0].toUpperCase();
+    return <div className={css.avatarPlaceholder}>{letter}</div>;
   };
 
   return (
-    <>
-      <Link onClick={toggleModal} className={css.userButton}>
-        <span className={css.userName}>Name</span>
-        {/* { {getUserAvatar()} */}
-        <UserAvatar />
-        <ReactSVG src={dropdownIcon} className={css.dropdownIcon} />
-        {isOpen && <UserLogoModal onClose={toggleModal} user={user} />}
-      </Link>
-    </>
+    <Link onClick={toggleModal} className={css.userButton}>
+      <span className={css.userName}>{user.name || user.email}</span>
+      {getUserAvatar()}
+      <ReactSVG src={dropdownIcon} className={css.dropdownIcon} />
+      {isOpen && <UserLogoModal onClose={toggleModal} user={user} />}
+    </Link>
   );
 };
-
-// {
-//   user.name || user.email;
-// }
 
 export default UserLogo;
