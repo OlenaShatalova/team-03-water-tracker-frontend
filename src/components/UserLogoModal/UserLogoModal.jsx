c6057f51d63686708be89fb2e6f98b8a4f640244
Juliia Prokopenko <atlasovaa13@gmail.com>
2025-02-08 18:12:36 +0100

    add icons, userlogomodal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
+import SettingModal from '../SettingModal/SettingModal';
+import css from './UserLogoModal.module.css';
+//import UserLogoutModal from '../UserLogoutModal/UserLogoutModal';
+import { ReactSVG } from 'react-svg';
+import ulgSetting from '../../assets/icons/ulg-setting.svg';
+import ulgLogout from '../../assets/icons/ulg-logout.svg';
 
-const UserLogoModal = ({ onClose, user }) => {
+const UserLogoModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Функції для відкриття та закриття модалки взята з SettingModal
+  const onOpenModal = () => {
+    setIsModalOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const onCloseModal = () => {
+    setIsModalOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
   return (
-    <div>
-      <button>Setting</button>
-      <button>Log out</button>
-    </div>
+    <>
+      <div className={css.wrapper}>
+        <ul className={css.UserLogoMenu}>
+          <li>
+            <button
+              type="button"
+              onClick={onOpenModal} // Открываем SettingModal
+              className={css.UserLogoButton}
+            >
+              <ReactSVG src={ulgSetting} className={css.ulgSetting} />
+              Setting
+            </button>
+          </li>
+          <li>
+            <button type="button" className={css.UserLogoButton}>
+              <ReactSVG src={ulgLogout} className={css.ulgLogout} />
+              Log out
+            </button>
+          </li>
+        </ul>
+        {/* Если activeModal === 'setting', показываем SettingModal */}
+        {isModalOpen && <SettingModal onClose={onCloseModal} />}
+      </div>
+    </>
   );
 };
 
