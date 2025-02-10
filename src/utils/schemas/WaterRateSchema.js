79bcb515100c23802328cb138a691cad41e5c630
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2025-02-10 18:59:17 +0200

    Update styles, add scroll

--- src/utils/schemas/WaterRateSchema.js
+++ src/utils/schemas/WaterRateSchema.js
@@ -6,22 +6,22 @@ export const validationSchema = Yup.object({
     .typeError('Must be a number')
     .positive('Must be a positive number')
     .min(20, 'Must be at least 20 kg')
-    .max(400, 'Must be less than 400 kg')
-    .required('Required'),
+    .max(400, 'Must be less than 400 kg'),
+  // .required('Required'),
   sportTimes: Yup.number()
     .typeError('Must be a number')
     .min(0, 'Cannot be negative')
-    .max(24, 'Too many hours')
-    .required('Required'),
+    .max(24, 'Too many hours'),
+  // .required('Required'),
   finalNumber: Yup.number()
     .typeError('Must be a number')
     .min(0, 'Cannot be negative')
-    .max(15, 'Too much water')
-    .required('Required')
-    .test(
-      'is-decimal',
-      'Must be a valid number (e.g., 1.5, 2.3)',
-      value =>
-        value === undefined || /^(\d+(\.\d{1,2})?)$/.test(value.toString())
-    ),
+    .max(15, 'Too much water'),
+  // .required('Required'),
+  // .test(
+  //   'is-decimal',
+  //   'Must be a valid number (e.g., 1.5, 2.3)',
+  //   value =>
+  //     value === undefined || /^(\d+(\.\d{1,2})?)$/.test(value.toString())
+  // ),
 });
