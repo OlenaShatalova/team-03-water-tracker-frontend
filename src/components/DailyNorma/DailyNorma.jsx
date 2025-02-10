6eb714649de8217b4b3518f3a85f025d8f964581
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2025-02-10 11:42:53 +0300

    add my daily norma 1.5 L

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,30 +1,34 @@
-import { useDispatch, useSelector } from "react-redux"
-import { openModal } from "../../redux/water/waterSlice"
-import { selectIsWaterRateModalOpen, selectWaterRateNumber } from "../../redux/water/waterSelectors";
-import DailyNormaModal from "../DailyNormaModal/DailyNormaModal";
-import css from "./DailyNorma.module.css"
-
+import { useDispatch, useSelector } from 'react-redux';
+import { openModal } from '../../redux/water/waterSlice';
+import {
+  selectIsWaterRateModalOpen,
+  selectWaterRateNumber,
+} from '../../redux/water/waterSelectors';
+import DailyNormaModal from '../DailyNormaModal/DailyNormaModal';
+import css from './DailyNorma.module.css';
 
 const DailyNorma = () => {
-    const dispatch = useDispatch();
-    const isOpen = useSelector(selectIsWaterRateModalOpen);
-    const waterRate = useSelector(selectWaterRateNumber);
+  const dispatch = useDispatch();
+  const isOpen = useSelector(selectIsWaterRateModalOpen);
+  const waterRate = useSelector(selectWaterRateNumber);
 
-    const onOpenModal = () => {
-        dispatch(openModal("isWaterRateOpen"))
-        console.log("Modal window is opened!")
-    }
+  const onOpenModal = () => {
+    dispatch(openModal('isWaterRateOpen'));
+    console.log('Modal window is opened!');
+  };
 
-    return (
-        <div className={css.container}>
-            <h3 className={css.title}>My daily norma</h3>
-            <div className={css.containerForNumbers}>
-                <div className={css.number}>{waterRate} L</div>
-                <button onClick={onOpenModal} className={css.button}>Edit</button>
-            </div>
-            {isOpen && <DailyNormaModal />}
-        </div>
-    )
-}
+  return (
+    <div className={css.container}>
+      <h3 className={css.title}>My daily norma</h3>
+      <div className={css.containerForNumbers}>
+        <div className={css.number}>{waterRate ? waterRate : '1.5 L'}</div>
+        <button onClick={onOpenModal} className={css.button}>
+          Edit
+        </button>
+      </div>
+      {isOpen && <DailyNormaModal />}
+    </div>
+  );
+};
 
-export default DailyNorma
+export default DailyNorma;
