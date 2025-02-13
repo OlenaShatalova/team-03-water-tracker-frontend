61d21c5f4ed64a5f8269933c49eae055f21639cc
Hanna Bondarenko <annaleshhuk@gmail.com>
2025-02-13 12:26:54 +0200

    fixed the themeBtn button in the header

--- src/components/ThemeBtn/ThemeBtn.jsx
+++ src/components/ThemeBtn/ThemeBtn.jsx
@@ -1,31 +1,27 @@
 import useTheme from '../ThemeContext/useTheme';
 import { BsSun, BsMoon } from 'react-icons/bs';
 import css from './ThemeBtn.module.css';
-import Container from '../Container/Container';
+// import Container from '../Container/Container';
 
 const ThemeBtn = () => {
   const { theme, toggleTheme } = useTheme();
   // console.log('Current theme:', theme);
 
   return (
-    <Container>
-      <button
-        className={css.themeButton}
-        onClick={toggleTheme}
-        aria-label="Toggle theme"
+    <button
+      className={css.themeButton}
+      onClick={toggleTheme}
+      aria-label="Toggle theme"
+    >
+      <span
+        className={`${css.icon} ${theme === 'light' ? css.show : css.hide}`}
       >
-        <span
-          className={`${css.icon} ${theme === 'light' ? css.show : css.hide}`}
-        >
-          <BsSun />
-        </span>
-        <span
-          className={`${css.icon} ${theme === 'dark' ? css.show : css.hide}`}
-        >
-          <BsMoon />
-        </span>
-      </button>
-    </Container>
+        <BsSun />
+      </span>
+      <span className={`${css.icon} ${theme === 'dark' ? css.show : css.hide}`}>
+        <BsMoon />
+      </span>
+    </button>
   );
 };
 
